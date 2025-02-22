95a448a890ef5d66cb7177b21c4fe8dddb5d81f9
Shantanu <shantanupathak01@gmail.com>
2025-02-22 15:27:47 +0530

    first commit

--- src/component/main/main.jsx
+++ src/component/main/main.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./main.scss"
 function Main() {
+    const [tottle,settoggle]=useState(false)
+    const handel=()=>{
+        settoggle(!tottle)
+    }
   return (
     <div className='main_continer'>
-        <div className='top_bar'></div>
+        <div className={`${tottle?"side_Menu":""}`}>
+
+        </div>
+
+        <div>
+        <div className='top_bar'>
+          <button onClick={handel}>click</button>
+        </div>
         <div className='profile_continer'>
         <div className='profile'>
             <div className='profile_iMG'>
@@ -12,15 +23,19 @@ function Main() {
             <label>Arun,29</label>
         </div>
         <div className='profile'>
-            <div className='profile_iMG'></div>
+            <div className='profile_iMG'>
+            <img className='profile_pic' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEth86FhV_U_AIi9tWrWo3ix-BRB7vSwwtXQ&s'/>
+            </div>
             <label>Arun,29</label>
         </div>
         <div className='profile'>
-            <div className='profile_iMG'></div>
+            <div className='profile_iMG'>
+            <img className='profile_pic' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEth86FhV_U_AIi9tWrWo3ix-BRB7vSwwtXQ&s'/>
+            </div>
             <label>Arun,29</label>
         </div>
         </div>
-       
+        </div>
     </div>
   )
 }
