d1065bd1241fea6af159758b0eb4387a0e0fe7d9
Shantanu <shantanupathak01@gmail.com>
2025-02-22 14:11:39 +0530

    first commit

--- src/component/main/main.jsx
+++ src/component/main/main.jsx
@@ -6,7 +6,9 @@ function Main() {
         <div className='top_bar'></div>
         <div className='profile_continer'>
         <div className='profile'>
-            <div className='profile_iMG'></div>
+            <div className='profile_iMG'>
+                <img className='profile_pic' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEth86FhV_U_AIi9tWrWo3ix-BRB7vSwwtXQ&s'/>
+            </div>
             <label>Arun,29</label>
         </div>
         <div className='profile'>
