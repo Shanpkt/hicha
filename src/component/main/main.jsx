8de9ee1d29d82dd599cfb4a94d691715d940bd14
Shantanu <shantanupathak01@gmail.com>
2025-02-22 11:44:13 +0530

    first commit

--- src/component/main/main.jsx
+++ src/component/main/main.jsx
@@ -4,6 +4,16 @@ function Main() {
   return (
     <div className='main_continer'>
         <div className='top_bar'></div>
+        <div className='profile_continer'>
+        <div className='profile'>
+            <div className='profile_iMG'></div>
+            <label>Arun,29</label>
+        </div>
+        <div className='profile'>
+            <div className='profile_iMG'></div>
+            <label>Arun,29</label>
+        </div>
+        </div>
     </div>
   )
 }
